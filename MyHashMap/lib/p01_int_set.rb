ed2368f233204e7638a4f64c0a491692ee5bdf75
Jose Mejia <josemejia247@gmail.com>
2020-11-23 12:00:00 -0500

    finished p1 int set

--- MyHashMap/lib/p01_int_set.rb
+++ MyHashMap/lib/p01_int_set.rb
@@ -33,15 +33,25 @@ end
 class IntSet
   def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { Array.new }
+    @num_buckets = num_buckets
   end
 
   def insert(num)
+    idx = num % @num_buckets
+    @store[idx] << num
   end
 
   def remove(num)
+    idx = num % @num_buckets
+    @store[idx].delete(num)
   end
 
   def include?(num)
+    idx = num % @num_buckets
+    @store[idx].each do |el|
+      return true if el == num
+    end
+    false
   end
 
   private
@@ -61,15 +71,32 @@ class ResizingIntSet
   def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { Array.new }
     @count = 0
+    @num_buckets = num_buckets
   end
 
   def insert(num)
+    return if include?(num)
+    if @store.length == @count
+      resize!
+    end
+    idx = num % @num_buckets
+    @store[idx] << num
+    @count += 1
   end
 
   def remove(num)
+    return if self.include?(num) == false
+    idx = num % @num_buckets
+    @store[idx].delete(num)
+    @count -= 1
   end
 
   def include?(num)
+    idx = num % @num_buckets
+    @store[idx].each do |el|
+      return true if el == num
+    end
+    false
   end
 
   private
@@ -83,5 +110,15 @@ class ResizingIntSet
   end
 
   def resize!
+    new_size = @count * 2
+    @num_buckets = new_size
+    new_store = Array.new(new_size) {Array.new}
+    @store.each do |sub|
+      sub.each do |ele|
+        idx = ele % @num_buckets
+        new_store[idx] << ele
+      end
+    end
+    @store = new_store
   end
 end
