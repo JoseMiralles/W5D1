913f9e9c85850dcdb02ce4a477ce74f02b57da4a
Jose Mejia <josemejia247@gmail.com>
2020-11-23 14:58:01 -0500

    finished part 3

--- MyHashMap/lib/p03_hash_set.rb
+++ MyHashMap/lib/p03_hash_set.rb
@@ -4,15 +4,32 @@ class HashSet
   def initialize(num_buckets = 8)
     @store = Array.new(num_buckets) { Array.new }
     @count = 0
+    @num_buckets = num_buckets
   end
 
   def insert(key)
+    return if include?(key)
+    if @store.length == @count
+      resize!
+    end
+    idx = key.hash % @num_buckets
+    @store[idx] << key
+    @count += 1
   end
 
   def include?(key)
+    idx = key.hash % @num_buckets
+    @store[idx].each do |el|
+      return true if el == key
+    end
+    false
   end
 
   def remove(key)
+    return if self.include?(key) == false
+    idx = key.hash % @num_buckets
+    @store[idx].delete(key)
+    @count -= 1
   end
 
   private
@@ -26,5 +43,15 @@ class HashSet
   end
 
   def resize!
+    new_size = @count * 2
+    @num_buckets = new_size
+    new_store = Array.new(new_size) {Array.new}
+    @store.each do |sub|
+      sub.each do |ele|
+        idx = ele.hash % @num_buckets
+        new_store[idx] << ele
+      end
+    end
+    @store = new_store
   end
 end
